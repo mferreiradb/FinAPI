2f75da5946c2526dd284c60c16583dce48042d54
Mauricio <mauhmg@gmail.com>
2022-12-28 22:31:15 -0300

    Criação da rota de cadastro e verificação de validação

--- src/index.js
+++ src/index.js
@@ -7,6 +7,27 @@ app.get('/', (req, res) => {
   res.json({msg: "Online"})
 })
 
+app.post('/create/acount', (req, res) => {
+  const { name, cpf } = req.body
+  const cpfList = ["05371957340"]
+  if (cpfList.length == 0 || null) {
+    cpfList.push(cpf)
+    console.log(cpfList)
+    return res.json({cpfList: cpfList})
+  } else {
+    for (let CPF in cpfList) {
+      if (cpf == cpfList[CPF]) {
+        console.log('CPF já cadastrado')
+        return res.json({erro: 'CPF já cadastrado'})
+      } else {
+        cpfList.push(CPF)
+        console.log(cpfList)
+        return res.json({cpfList: cpfList})
+      }
+    }
+  }
+})
+
 app.listen(8080, () => {
   console.log("Servidor online na url http://localhost:8080");
 });
