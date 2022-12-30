9bc1d8e0708663856cb2dbd243ecd61bbff8a971
Mauricio <mauhmg@gmail.com>
2022-12-29 21:21:48 -0300

    Post de deposito

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ const customers = [
     name: "Mauricio",
     cpf: "0000000000",
     id,
-    statement: ["0000000000", "11111111111111"],
+    statement: [{ description: "Deposito Mauricio", amount: 57.5 }, { description: 'Deposito', amount: 50 }],
   },
 ];
 
+//MIDDLEWARE PARA AUTENTICAÇÃO DE EXISTENCIA DE CONTA
 const acountAuth = (req, res, next) => {
-  const { cpf } = req.params;
+  const { cpf } = req.headers;
   const customer = customers.find((customer) => customer.cpf == cpf);
   if (!customer) {
     return res.status(400).json({ error: "Conta não encontrada" });
@@ -44,10 +45,20 @@ app.post("/create/acount", (req, res) => {
   }
 });
 
-app.get("/statement/:cpf", acountAuth, (req, res) => {
-    const { customer } = res
-    console.log("Statement: " + customer.statement);
-    return res.json(customer.statement);
+app.get("/statement/", acountAuth, (req, res) => {
+  const { customer } = res;
+  console.log("Statement: " + customer.statement);
+  return res.json(customer.statement);
+});
+
+app.post("/deposit", acountAuth, (req, res) => {
+  const { description, amount } = req.body;
+  const { customer } = res;
+  const deposit = {description, amount, createdAt: new Date().toLocaleString(), type: 'credit'}
+
+  customer.statement.push(deposit);
+  console.log(customer.statement);
+  return res.status(201).json({ msg: "Valor adicionado" });
 });
 
 app.listen(8080, () => {
